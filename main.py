277fdd79971855067a2df07522e216668a48b22f
jeas560 <jeas560@gmail.com>
2022-04-13 14:10:30 -0300

    removing main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas
-
-# with open('desafio-base1/agenda.csv', encoding='iso-8859-1') as csv_file:
-#     csv_reader = csv.DictReader(csv_file, delimiter='|')
-#     print(csv_reader.keys())
-
-df = pandas.read_csv('desafio-base1/agenda.csv', encoding='iso-8859-1',quotechar='"', delimiter='|')
-df['CodConvenio']
-print(df)   
